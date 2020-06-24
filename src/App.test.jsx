8afa4335482101e4a7735cab3db7ff20633a5cfd
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-24 13:03:26 +0900

    tests(InputContainer, ListContainer): add test cases
    - 강의 내용 실습 및 테스트 코드 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,28 +2,31 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
 jest.mock('react-redux');
 
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   const tasks = [
-    { id: 1, title: '아무것도 하지 않기 1' },
-    { id: 2, title: '아무것도 하지 않기 2' },
+    { id: 1, title: 'Task-1' },
+    { id: 2, title: 'Task-2' },
   ];
 
   useSelector.mockImplementation((selector) => selector({
     tasks,
+    taskTitle: '',
   }));
 
   const { getByText } = render((
     <App />
   ));
 
-  expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  expect(getByText(/Task-1/)).not.toBeNull();
+  expect(getByText(/Task-2/)).not.toBeNull();
 });
