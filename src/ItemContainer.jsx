b1594148e5530e21b41dc9b76689f28992c27453
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-25 17:25:11 +0900

    feat(ItemContainer): use redux
    - 리덕스를 사용해서 상태값을 불러오고 렌더링하는 기능 구현

--- src/ItemContainer.jsx
+++ src/ItemContainer.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function ItemContainer() {
+  const { task } = useSelector((state) => ({
+    task: state.task,
+  }));
+
   return (
     <div>
-      완료
+      <div>
+        완료
+      </div>
+      <div>
+        {task.title}
+      </div>
     </div>
   );
 }
