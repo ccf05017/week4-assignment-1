8716e65b38e085d9e1d0da822ad079ab97897278
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-25 17:39:19 +0900

    test(ItemContainer): onClickDelete
    - 완료 버튼을 눌렀을 때 기대되는 이벤트 테스트 케이스 추가

--- src/ItemContainer.test.jsx
+++ src/ItemContainer.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ItemContainer from './ItemContainer';
 
@@ -27,5 +27,28 @@ describe('ItemContainer', () => {
 
     expect(getByText(/완료/)).not.toBeNull();
     expect(getByText(title)).not.toBeNull();
+  });
+
+  it('can click button', () => {
+    const title = '아무것도 안하기';
+
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+
+    const task = {
+      id: 100,
+      title,
+    };
+
+    useSelector.mockImplementation((selector) => selector({
+      task,
+    }));
+
+    const { getByText } = render((
+      <ItemContainer />
+    ));
+
+    fireEvent.click(getByText(/완료/));
+    expect(dispatch).toBeCalledWith(title);
   });
 });
