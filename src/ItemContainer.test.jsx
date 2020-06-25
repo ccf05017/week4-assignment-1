82625e6c00ad1553dfd9d99617653598d2cf4037
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-25 17:19:33 +0900

    test(ItemContainer): see content
    - 할 일이 있을 때 할일의 제목을 볼 수 있는 테스트 추가

--- src/ItemContainer.test.jsx
+++ src/ItemContainer.test.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import ItemContainer from './ItemContainer';
+
+jest.mock('react-redux');
 
 describe('ItemContainer', () => {
   it('can see component', () => {
+    const title = '아무것도 안하기';
+
+    const task = {
+      id: 100,
+      title,
+    };
+
+    useSelector.mockImplementation((selector) => selector({
+      task,
+    }));
+
     const { getByText } = render((
       <ItemContainer />
     ));
 
     expect(getByText(/완료/)).not.toBeNull();
+    expect(getByText(title)).not.toBeNull();
   });
 });
